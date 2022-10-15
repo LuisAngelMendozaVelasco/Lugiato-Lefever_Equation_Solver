8072d7014715ebe536060c422070f790a0f85393
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-15 15:10:19 -0500

    Update Ring_core.py
    Update def __init__

--- Ring_core.py
+++ Ring_core.py
@@ -1,4 +1,5 @@
-from scipy.constants import c, hbar
+from scipy.constants import c, hbar, pi
+import numpy as np
 
 class Ring:
     def __init__(self, ring_param) -> None:
@@ -12,4 +13,18 @@ class Ring:
         self.Veff = ring_param['Veff']
         self.D2 = ring_param['D2']
         self.n2T = ring_param['n2T'] 
-        self.Pin = ring_param['Pin']          
+        self.Pin = ring_param['Pin']
+
+        # Calculate further parameters
+        self.Tr = 1 / self.FSR # Roundtrip time [s]
+        self.freq0 = c / self.lambda0 # CW pump frequency [Hz]
+        self.omega0 = 2 * pi * self.freq0 # CW pump angular frequency [rad/s]
+        self.g = hbar * self.omega0**2 * c * self.n2 / (self.n0**2 * self.Veff) # Nonlinear coupling coefficient
+        self.kappa = 2 * pi * self.kappa # Optical linewidth [rad/s]
+        self.f = np.sqrt((self.Pin / (hbar * self.omega0)) * (8 * self.g * self.eta / self.kappa**2)) # Normalized pump field
+        self.f = np.insert(np.zeros(self.N), int((self.N / 2) + 1), self.f) # Normalized pump field vector
+        self.D2 = 2 * pi * self.D2 # Second order dispersion [rad/s]
+        self.d2 = (2 / self.kappa) * self.D2 # Normalized second order dispersion
+        self.mu = np.arange(-(self.N - 1) / 2, ((self.N - 1) / 2) + 1, 1) # Mode numbers relative to the pumped mode
+        self.dint = (self.d2 / 2) * self.mu**2 # Normalized integrated dispersion
+        
