2273e56fdf3751cb265a1e3b0cdce5144c550a73
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-17 16:54:57 -0500

    Update Ring_core.py
    Create function for plotting results

--- Ring_core.py
+++ Ring_core.py
@@ -3,6 +3,10 @@ from scipy.fft import ifft, fft
 from scipy.integrate import solve_ivp
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('whitegrid')
 
 class Ring:
     def __init__(self, ring_param) -> None:
@@ -59,3 +63,55 @@ class Ring:
             amu[i + 1, :] = solver.y[:, -1] # Store field solution
         
         return dseta, amu
+
+    def plot_res(self, dseta_fwrd, amu_fwrd, dseta_snap):
+        fig = plt.figure(figsize=(14, 14))
+        plt.rcParams['font.size'] = '18'
+
+        avg_intr_int_fwrd = np.sum(np.abs(amu_fwrd)**2, 1) # Average intracavity intensity of forward tuning
+        ax1 = plt.subplot(311)
+        ax1.plot(dseta_fwrd, avg_intr_int_fwrd, label='Forward tuning', linewidth=2)
+        ax1.axvline(dseta_snap, color='black', linestyle='--', linewidth=2, label='$\zeta$ = {}'.format(dseta_snap)) # Plot dseta_snap
+        ax1.set_xlabel('Normalized detuning, $\zeta$')
+        ax1.set_ylabel('Average intracavity intensity [a. u.]')
+        ax1.set_xlim(np.min(dseta_fwrd), np.max(dseta_fwrd))
+        ax1.set_ylim(0, np.max(avg_intr_int_fwrd) * 1.05)
+        ax1.legend()
+
+        idx_fwrd = np.abs(dseta_fwrd - dseta_snap).argmin()
+
+        omegamu = self.omega0 + (2 * pi * self.FSR * self.mu) + (2 * pi * self.D2 * self.mu**2 / 2)
+        freqmu = omegamu / (2 * pi) # Resonance frequencies [Hz]
+        fout = self.f - 2 * self.eta * np.abs(amu_fwrd[idx_fwrd, :])
+        fin = np.max(self.f) * np.ones(self.N)
+        opt_spec = (np.abs(fout)**2 / np.abs(fin)**2) * self.Pin
+        opt_spec = 10 * np.log10(1000 * opt_spec)
+        ax2 = plt.subplot(323)
+        ax2.stem(freqmu * 1e-12, opt_spec, markerfmt=",", bottom=-60)
+        ax2.set_xlabel('Resonance frequencies [THz], $f_\mu$')
+        ax2.set_ylabel('Optical spectrum [dBm]')
+        ax2.set_xlim(freqmu[0] * 1e-12, freqmu[-1] * 1e-12)
+        ax2.set_ylim(-60, 10 * np.log10(1000 * self.Pin) * 1.05)
+
+        ax3 = plt.subplot(324)
+        ax3.set_title('3')
+
+        ring_circum = np.linspace(-pi, pi, 4096)
+        temp = np.zeros_like(ring_circum, dtype=np.complex_)
+        temp[np.int32(self.mu + (4096 / 2) + 1)] = amu_fwrd[idx_fwrd, :]
+        intr_pow = np.abs(fft(temp))**2
+        ax4 = plt.subplot(325)
+        ax4.plot(ring_circum, intr_pow, linewidth=2)
+        ax4.set_xlabel('Ring circumference [rad], $\phi$')
+        ax4.set_ylabel('Intracavity power [a. u]')
+        ax4.set_xlim(-pi, pi)
+        ax4.set_ylim(0, np.max(intr_pow) * 1.05)
+        ax4.set_xticks([-pi, -pi/2, 0, pi/2, pi])
+        ax4.set_xticklabels(['$-\pi$', '$-\pi$/2', '0', '$\pi$/2', '$\pi$'])
+
+        ax5 = plt.subplot(326)
+        ax5.set_title('5')
+
+        fig.canvas.header_visible = False
+        plt.tight_layout()
+        plt.show()
