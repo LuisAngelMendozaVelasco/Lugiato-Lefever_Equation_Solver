f1adf4662a0c2f12f151241305ee44ab11d51621
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-16 21:20:35 -0500

    Update Ring_core.py
    Create function for numerical simulation

--- Ring_core.py
+++ Ring_core.py
@@ -1,5 +1,8 @@
 from scipy.constants import c, hbar, pi
+from scipy.fft import ifft, fft
+from scipy.integrate import solve_ivp
 import numpy as np
+import time
 
 class Ring:
     def __init__(self, ring_param) -> None:
@@ -21,10 +24,50 @@ class Ring:
         self.omega0 = 2 * pi * self.freq0 # CW pump angular frequency [rad/s]
         self.g = hbar * self.omega0**2 * c * self.n2 / (self.n0**2 * self.Veff) # Nonlinear coupling coefficient
         self.kappa = 2 * pi * self.kappa # Optical linewidth [rad/s]
-        self.f = np.sqrt((self.Pin / (hbar * self.omega0)) * (8 * self.g * self.eta / self.kappa**2)) # Normalized pump field
-        self.f = np.insert(np.zeros(self.N), int((self.N / 2) + 1), self.f) # Normalized pump field vector
+        self.f = np.zeros(self.N) # Normalized pump field vector
+        self.f[int((self.N / 2) + 1)] = np.sqrt((self.Pin / (hbar * self.omega0)) * (8 * self.g * self.eta / self.kappa**2)) # Normalized pump field
         self.D2 = 2 * pi * self.D2 # Second order dispersion [rad/s]
         self.d2 = (2 / self.kappa) * self.D2 # Normalized second order dispersion
         self.mu = np.arange(-(self.N - 1) / 2, ((self.N - 1) / 2) + 1, 1) # Mode numbers relative to the pumped mode
         self.dint = (self.d2 / 2) * self.mu**2 # Normalized integrated dispersion
+    
+    def num_sim(self, params, sim_opts):
+        # Retrieve simulation parameters
+        dseta_start = params['dseta_start']
+        dseta_end = params['dseta_end'] 
+        dseta_step = params['dseta_step']
+        roundtrips_step = params['roundtrips_step']
+        Amu0 = params['Amu0'] 
+
+        amu0 = ifft(np.sqrt(2 * self.g / self.kappa) * Amu0) # Normalized initial field
+        self.dseta = np.arange(dseta_start, dseta_end + dseta_step, dseta_step) # Normalized detuning (vector)
+        self.tau_step = (self.kappa / 2) * roundtrips_step * self.Tr # Normalized time per tuning step
+
+        amu = np.zeros((len(self.dseta), self.N), dtype=np.complex_) # 2D array to store normalized fields
+        amu[0, :] = amu0 # Store normalized initial field
+        progress = 0 # Variable to store simulation progress
+        print('Progress: {}%'.format(progress))
+
+        time_start = time.time() # Time when for loop starts
+        for i in range(len(self.dseta) - 1):
+            curr_prog = round(((i + 1) / (len(self.dseta) - 1)) * 100) # Current simulation progress
+            if progress < curr_prog:
+                progress = curr_prog # Update simulation progress
+                print('Progress: {}%'.format(progress))
+            dseta_curr = self.dseta[i] # Current detuning value
+            y0 = amu[i, :] # Initial conditions to solve LLE
+            if i == 0:
+                def LLE(tau, y):
+                    amu_ = y
+                    damu_ = -(1 + 1j * (dseta_curr + (dseta_step * (tau / self.tau_step)) + self.dint)) * amu_ + 1j * ifft(abs(fft(amu_))**2 * fft(amu_)) + self.f # LLE
+                    dy = damu_
+
+                    return dy 
+            solver = solve_ivp(LLE, [0, self.tau_step], y0)
+            amu[i + 1, :] = solver.y[:, -1]
+        time_end = time.time() # Time when for loop ends
+
+        print("Total time: {:0.2f} seconds". format(time_end - time_start))
+        
+        return amu
         
